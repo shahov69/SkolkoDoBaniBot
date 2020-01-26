d3087ba8a9e8f2981fa59e336cecc100faabb755
xander <xander.shahov@gmail.com>
2020-01-27 00:55:43 +0300

    add - миграция БД

--- src/main/resources/db/migration/V5__Populate_Kirushizms.sql
+++ src/main/resources/db/migration/V5__Populate_Kirushizms.sql
@@ -11,23 +11,19 @@ VALUES (6, 'Ява - говно.', '2019-02-12 21:42:17.291000', true, 'Алек
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
 VALUES (7, 'I''m T.N.T. I''m dynamite', '2019-02-13 08:45:25.291000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
-VALUES (8, 'Всё пиздец, Госдума приняла в первом чтении законопроект об изоляции российского сегмента интернета',
-        '2019-02-13 08:50:17.624000', true, 'Александр Шахов');
+VALUES (8, 'Всё пиздец, Госдума приняла в первом чтении законопроект об изоляции российского сегмента интернета', '2019-02-13 08:50:17.624000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
-VALUES (9, 'Всё пиздец, Матвиенко попросила Медведева полностью остановить экспорт леса', '2019-02-13 08:51:52.094000',
-        true, 'Александр Шахов');
+VALUES (9, 'Всё пиздец, Матвиенко попросила Медведева полностью остановить экспорт леса', '2019-02-13 08:51:52.094000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
 VALUES (11, 'Может хватит тут хрустеть уже?', '2019-02-14 16:55:42.447000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
 VALUES (12, 'Твой пруф - полное дерьмо.', '2019-02-14 17:03:02.232000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
-VALUES (13, 'Всё пиздец, Росавтодор покроет российские дороги "суперасфальтом"', '2019-02-14 17:04:09.568000', true,
-        'Александр Шахов');
+VALUES (13, 'Всё пиздец, Росавтодор покроет российские дороги "суперасфальтом"', '2019-02-14 17:04:09.568000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
 VALUES (17, 'Кловер навсегда! ♥️♥️♥️♥️♥️', '2019-02-15 08:02:05.441000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
-VALUES (18, 'Всё пиздец, Владимир Путин вышел на татами в Сочи.', '2019-02-15 08:05:24.167000', true,
-        'Александр Шахов');
+VALUES (18, 'Всё пиздец, Владимир Путин вышел на татами в Сочи.', '2019-02-15 08:05:24.167000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
 VALUES (19, 'Кловер - полное дерьмо.', '2019-02-15 08:07:42.532000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
@@ -35,8 +31,7 @@ VALUES (20, 'Расскажи хоть, как тебе хуёво.', '2019-02-1
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
 VALUES (21, 'Побыстрее бы на кловер перейти.', '2019-02-15 14:05:40.483000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
-VALUES (53, 'Я не знаю, что сказать, кроме того, что оракл мудаки.', '2019-02-26 15:01:41.822000', true,
-        'Александр Шахов');
+VALUES (53, 'Я не знаю, что сказать, кроме того, что оракл мудаки.', '2019-02-26 15:01:41.822000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
 VALUES (58, 'Anata ga dore dake warui no ka o oshietekudasai', '2019-04-29 21:26:10.971000', true, 'Александр Шахов');
 INSERT INTO kirushizm (id, txt, created, accepted, creator)
